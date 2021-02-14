5af44e40feec808c0056d3e0850d7ea922fa5dec
Arvind Ramgopal <arvind.ramgopal@gmail.com>
2021-02-14 12:06:13 -0800

    test pipeline again

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -146,7 +146,7 @@ export default function List(props) {
             } `}
             onClick={onClickAdd}
           >
-            Submits
+            Submit
           </a>
         </div>
         <Droppable droppableId="list-items__container">
