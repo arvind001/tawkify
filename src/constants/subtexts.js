3f423b1f92757cbb5c2ebc2c9611e00c4bd2090b
Arvind Ramgopal <arvind.ramgopal@gmail.com>
2021-02-16 11:47:12 -0800

    Changed subtext for max

--- src/constants/subtexts.js
+++ src/constants/subtexts.js
@@ -1,7 +1,7 @@
 export const SUBTEXTS = {
   disabled: "This field is disabled.",
   required: "This field is required. Please add an item.",
-  max: "List reached limit. Delete item to add a new one.",
+  max: "List limit reached. Delete item to add a new one.",
   duplicate: "This item is already in the list.",
   none: "",
 };
