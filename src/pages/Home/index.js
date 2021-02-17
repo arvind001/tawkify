d345f4bac040741d9fdb31df1405219b5a791c74
Arvind Ramgopal <arvind.ramgopal@gmail.com>
2021-02-17 00:17:23 -0800

    remove unecessary things and change checkbox styles

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,15 +6,15 @@ import { store } from "../../store/store";
 import "./style.css";
 
 export default function Home(props) {
-  const gState = useContext(store);
-  const { state } = gState;
+  const { state } = useContext(store);
+  //   const { state } = gState;
   var max = state.max !== "" ? parseInt(state.max) : null;
   return (
     <div className="col">
       <Header />
       <div className="home__container row">
         <div className="list-controller__container col">
-          <div className="home__heading">Input Properties Controller</div>
+          <div className="home__heading">Properties Controller</div>
           <Controller />
         </div>
         <div className="home__list__container col">
